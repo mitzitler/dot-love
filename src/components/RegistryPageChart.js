a6ed32dcca438e06f10f077c9eb96e10d4ea9bd5
Mitzi Zitler <mitzizitler@Mitzis-MacBook-Pro.local>
2024-09-01 01:12:59 -0400

    Removed all extraneous code from D3 registry chart logic

--- src/components/RegistryPageChart.js
+++ src/components/RegistryPageChart.js
@@ -1,48 +1,18 @@
-import { ResponsiveScatterPlot } from "@nivo/scatterplot";
-import { useState, useRef, useEffect } from "react";
-import { VictoryContainer, VictoryChart, VictoryAxis, VictoryScatter, VictoryTheme, VictoryTooltip, VictoryVoronoiContainer } from "victory";
 import * as d3 from "d3";
 import { scaleLinear } from "d3-scale";
 
-const gift_data_victory = [
-    { x: 1,   y: 2,   label: "This gift", id: 1 },
-    { x: 2,   y: 3,   label: "Another gift", id: 2 },
-    { x: 3,   y: 5,   label: "Something else", id: 3 },
-    { x: 4,   y: 4,   label: "Buy me", id: 4 },
-    { x: 5,   y: 7,   label: "Sale Sale Sale", id: 5 },
-    { x: -10, y: 10,  label: "That gift", id: 6 },
-    { x: 10,  y: 10,  label: "Dont forget to resgister", id: 7 },
-    { x: -10, y: -10, label: "Present", id: 8 },
-    { x: 10,  y: -10, label: "Wedding gift", id: 9 },
+const gift_data = [
+    { x: 1,   y: 2,   id: 1, name: "Teapot"  },
+    { x: 2,   y: 3,   id: 2, name: "Toaster" },
+    { x: 3,   y: 5,   id: 3, name: "Towels" },
+    { x: 4,   y: 4,   id: 4, name: "Painting" },
+    { x: 5,   y: 7,   id: 5, name: "Cups" },
+    { x: -10, y: 10,  id: 6, name: "Forks" },
+    { x: 10,  y: 10,  id: 7, name: "Wine" },
+    { x: -10, y: -10, id: 8, name: "Table" },
+    { x: 10,  y: -10, id: 9, name: "Toy" },
 ]
 
-const gift_data_info = [
-    { id: 1, name: "Teapot" },
-    { id: 2, name: "Toaster" },
-    { id: 3, name: "Towels" },
-    { id: 4, name: "Painting" },
-    { id: 5, name: "Cups" },
-    { id: 6, name: "Forks" },
-    { id: 7, name: "Wine" },
-    { id: 8, name: "Table" },
-    { id: 9, name: "Toy" },
-]
-
-const gift_data_d3 = [
-    { x: 1,   y: 2,   id: 1 },
-    { x: 2,   y: 3,   id: 2 },
-    { x: 3,   y: 5,   id: 3 },
-    { x: 4,   y: 4,   id: 4 },
-    { x: 5,   y: 7,   id: 5 },
-    { x: -10, y: 10,  id: 6 },
-    { x: 10,  y: 10,  id: 7 },
-    { x: -10, y: -10, id: 8 },
-    { x: 10,  y: -10, id: 9 },
-]
-
-// react example
-// using 
-        // this site is doing this https://dev.to/julienassouline/let-s-get-started-with-react-and-d3-2nd7
 
 function AxisLeft({ yScale, width }) {
     const textPadding = -20
@@ -66,7 +36,7 @@ function AxisLeft({ yScale, width }) {
             </text>
         </g>
         ));
-        return <>{axis}</>;
+    return <>{axis}</>;
 };
 
 function AxisLeftZero({ yScale, width}) {
@@ -81,12 +51,11 @@ function AxisLeftZero({ yScale, width}) {
             />
         </g>
         ));
-        return <>{axis}</>;
+    return <>{axis}</>;
 }
         
 function AxisBottom({ xScale, height }) {
     const textPadding = 10;
-    
     const axis = xScale.ticks(10).map((d, i) => (
         <g className="x-tick" key={i}>
         <line
@@ -107,7 +76,7 @@ function AxisBottom({ xScale, height }) {
         </g>
     ));
     return <>{axis}</>;
-    };
+};
 
 function AxisBottomZero({ xScale, height }) {    
     const axis = xScale.ticks(10).map((d, i) => (
@@ -126,7 +95,7 @@ function AxisBottomZero({ xScale, height }) {
         
 
 export function RegistryChartD3( ) {
-        const data = gift_data_victory,
+    const data = gift_data,
         w = 500,
         h = 500,
         margin = {
@@ -159,305 +128,20 @@ export function RegistryChartD3( ) {
             fillOpacity={0.2}
             onMouseDown={() => console.log(d)}
         />
-        ));
+    ));
 
-        return (
-            <div>
-                <svg width={w} height={h}>
-                    <g transform={`translate(${margin.left},${margin.top})`}>
-                        <AxisLeft yScale={yScale} width={width}>
-                        </AxisLeft>
-                        <AxisBottom xScale={xScale} height={height} />
-                        <AxisLeftZero yScale={yScale} width={width} />
-                        <AxisBottomZero xScale={xScale} height={height} />
-                        {circles}
-                    </g>
-                </svg>
-            </div>
-        )
-    }
-
-
-// export function RegistryChartD3() {
-//     const data = useState([
-//         [1, 2],
-//         [2, 3],  
-//         [3, 5], 
-//         [4, 4], 
-//         [5, 7],
-//         [-10, 10],
-//         [10, 10],
-//         [-10, -10],
-//         [10, -10]
-//     ])
-
-//     const svgRef = useRef()
-
-
-//         // setting up the container
-//         const w = 500;
-//         const h = 300;
-//         const svg = d3.select(svgRef.current)
-//             .attr('width', w)
-//             .attr('height', h)
-//             .style('overflow', 'visible')
-//             .style('margin', '100px');
-
-//         // setting up the scaling
-//         const xScale = d3.scaleLinear()
-//             .domain([-10, 10])
-//             .range([0, w]);
-//         const yScale = d3.scaleLinear()
-//             .domain([10, -10])
-//             .range([0, h]);
-
-//         // setting up the axis
-//         const xAxis = d3.axisBottom(xScale).ticks(10);
-//         const yAxis = d3.axisLeft(yScale).ticks(10);
-//         svg.append('g')
-//             .call(xAxis)
-//             .attr('transform', `translate(0, ${h})`);
-//         svg.append('g')
-//             .call(yAxis);
-
-//         // setting up the axis labeling
-//         svg.append('text')
-//             .attr('x', w/2)
-//             .attr('y', h+50)
-//             .text('x');
-//         svg.append('text')
-//             .attr('y', h/2)
-//             .attr('x', -50)
-//             .text('y');
-
-//         // setting up svg data
-//         svg.selectAll()
-//             .data(data)
-//             .enter()
-//             .append('circles')
-//                 .attr('cx', d => xScale(d[0]))
-//                 .attr('cy', d => yScale(d[1]))
-//                 .attr('r', 4)
-//                 .attr('fill', '#78416f');
-        
-
-
-//     return (
-//         <div>
-//             <svg ref= {svgRef}> 
-//                 <g transform={`translate(${100},${100})`}>
-//                     {circles}
-//                 </g>
-//             </svg>
-//         </div>
-//     )
-// }
-
-
-
-// export function RegistryChartVictory() { 
-
-//     const [xyz, setXyz] = useState({externalMutations: undefined});
-//     const [selectedGift, setSelectedGift] = useState(null);
-    
-//     function removeMutation() {
-//         setXyz({
-//           externalMutations: undefined
-//         });
-//     }
-
-//     function clearClicks() {
-//         setXyz({
-//           externalMutations: [
-//             {
-//               childName: "Bar-1",
-//               target: ["data"],
-//               eventKey: "all",
-//               mutation: () => ({ style: undefined }),
-//               callback: removeMutation
-//             }
-//           ]
-//         });
-//     }
-
-//     function logSelectedGift() {
-//         setSelectedGift()
-//     }
-
-//     function showSelected() {
-//         setSelectedGift()
-//     }
-
-//     return(
-//         <div style={{ height: "400px", width: "800px" }}>
-//             <VictoryChart
-//                 containerComponent={<VictoryVoronoiContainer/>}
-//                 //theme={VictoryTheme.material}
-//                 domain={{ x: [-10, 10], y: [-10, 10] }}
-//                 // externalEventMutations={xyz.externalMutations}
-//                 // events={[
-//                 //     {
-//                 //         target: "data",
-//                 //         childName: "dot-1",
-//                 //         eventHandlers: {
-//                 //             onClick: () => ({
-//                 //                 target: "data",
-//                 //                 mutation: () => ({ style: { fill: "orange" } })
-//                 //             })
-//                 //         }
-//                 //     }
-//                 // ]}
-//                 >
-//                 {/* <VictoryAxis crossAxis
-//                     width={400}
-//                     height={400}
-//                     domain={[-10, 10]}
-//                     theme={VictoryTheme.material}
-//                     //offsetY={200}
-//                     standalone={false}
-//                 />
-//                 <VictoryAxis dependentAxis crossAxis
-//                     width={400}
-//                     height={400}
-//                     domain={[-10, 10]}
-//                     theme={VictoryTheme.material}
-//                     //offsetX={200}
-//                     standalone={false}
-//                 /> */}
-//                 <VictoryScatter
-//                     name = "dot-1"
-//                     size={(datum, active) => active ? 10 : 7}
-//                     labelComponent={<VictoryTooltip/>}
-//                     style={{ data: { fill: "#c43a31" }, labels: { fill: "#c43a31" } }}
-//                  //   size={7}
-//                     data={gift_data_victory}
-//                     // events={[{
-//                     //   target: "data",
-//                     //   eventHandlers: {
-//                     //     onMouseOver: () => {
-//                     //       return [
-//                     //         {
-//                     //           target: "data",
-//                     //           mutation: () => ({style: {fill: "gold", width: 30}})
-//                     //         }, {
-//                     //           target: "labels",
-//                     //           mutation: () => ({ active: true })
-//                     //         }
-//                     //       ];
-//                     //     },
-//                     //     onMouseOut: () => {
-//                     //       return [
-//                     //         {
-//                     //           target: "data",
-//                     //           mutation: () => {}
-//                     //         }, {
-//                     //           target: "labels",
-//                     //           mutation: () => ({ active: false })
-//                     //         }
-//                     //       ];
-//                     //     }
-//                     //   }
-//                     // }]}
-//                 />
-//             </VictoryChart>
-//         </div>
-//     )
-// }
-
-// const gift_data_nivo = 
-// [
-//     {
-//         "id": "bought",
-//         "data": [
-//             {
-//                 "x": -2,
-//                 "y": 7,
-//                 "z": 1
-//             },
-//             {
-//                 "x": 3,
-//                 "y": 1,
-//                 "z": 2
-//             }
-//         ],
-//     },
-//     {
-//         "id": "unbought",
-//         "data": [
-//             {
-//                 "x": -1,
-//                 "y": -4,
-//                 "z": 3
-//             },
-//             {
-//                 "x": -1.2,
-//                 "y": 9,
-//                 "z": 4
-//             }
-//         ],
-//     }
-// ]
-
-// export function RegistryChartNivo() {
-//     return(
-//     <div style={{ height: "400px", width: "800px" }}>
-//     <ResponsiveScatterPlot
-//         data={gift_data_nivo}
-//         margin={{ top: 60, right: 140, bottom: 70, left: 90 }}
-//         xScale={{ type: 'linear', min: -10, max: 10 }}
-//         xFormat=">-.2f"
-//         yScale={{ type: 'linear', min: -10, max: 10 }}
-//         yFormat=">-.2f"
-//         blendMode="multiply"
-//         axisTop={null}
-//         axisRight={null}
-//         axisBottom={{
-//             orient: 'bottom',
-//             tickSize: 2,
-//             tickPadding: 5,
-//             tickRotation: 0,
-//             legend: 'size',
-//             legendPosition: 'middle',
-//             legendOffset: 0,
-//             truncateTickAt: 0
-//         }}
-//         axisLeft={{
-//             orient: 'left',
-//             tickSize: 2,
-//             tickPadding: 5,
-//             tickRotation: 0,
-//             legend: 'function',
-//             legendPosition: 'middle',
-//             legendOffset: 0,
-//             truncateTickAt: 0
-//         }}
-        
-//         legends={[
-//             {
-//                 anchor: 'bottom-left',
-//                 direction: 'row',
-//                 justify: false,
-//                 translateX: 20,
-//                 translateY: 40,
-//                 itemWidth: 100,
-//                 itemHeight: 12,
-//                 itemsSpacing: 5,
-//                 itemDirection: 'left-to-right',
-//                 symbolSize: 12,
-//                 symbolShape: 'circle',
-//                 effects: [
-//                     {
-//                         on: 'hover',
-//                         style: {
-//                             itemOpacity: 1
-//                         }
-//                     }
-//                 ]
-//             }
-//         ]}
-//     />
-//     </div>
-//     )
-// }
-
-// //export default RegistryChartNivo;
+    return (
+        <div>
+            <svg width={w} height={h}>
+                <g transform={`translate(${margin.left},${margin.top})`}>
+                    <AxisLeft yScale={yScale} width={width}>
+                    </AxisLeft>
+                    <AxisBottom xScale={xScale} height={height} />
+                    <AxisLeftZero yScale={yScale} width={width} />
+                    <AxisBottomZero xScale={xScale} height={height} />
+                    {circles}
+                </g>
+            </svg>
+        </div>
+    )
+}
