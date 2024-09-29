57cbdb190df349c9684e6b385f6f0a9ab3e2d271
Mitzi Zitler <mitzitler@gmail.com>
2024-09-29 02:35:58 -0400

    Left a message on the ideal nav bar sizing

--- src/components/GenericHeader.js
+++ src/components/GenericHeader.js
@@ -6,12 +6,15 @@ export function GenericHeader() {
 
     return (
         <div class = "header-main">
+            {/* i am as tall as the inside of the rotating circle (not the outside) */}
             <span class = "header-side">
                 I am the left side
+                {/* i am as tall as the inner button */}
             </span>
             <NavIcon/> 
             <span class = "header-side">
                 I am the right side
+                {/* i am as tall as the inner button */}
             </span>
         </div>
     )
