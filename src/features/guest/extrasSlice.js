40fa9a8959c8b40bf691ede19f47be61130361e0
Mitzi Zitler <mitzitler@gmail.com>
2025-05-01 00:54:05 -0400

    redux for login header

--- src/features/guest/extrasSlice.js
+++ src/features/guest/extrasSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 const extrasSlice = createSlice({
   name: "extras",
   initialState: {
+    loginHeaderState: '',
     giftClaimed: {}, // id: , claimed: boolean, 
   },
   reducers: {
@@ -12,10 +13,14 @@ const extrasSlice = createSlice({
       // toggle the claimed state to opposite
       state.giftClaimed[action.payload[0]] = !action.payload[1] 
       console.log('for item id', action.payload[0], 'the claimed state is now', action.payload[1])
+    },
+    setloginHeaderState(state, action) {
+      state.loginSuccessState = action.payload
+      console.log('The login header for this session is: ', action.payload)
     }
   }
 })
 
-export const { giftClaimedToggle } = extrasSlice.actions
+export const { giftClaimedToggle, setloginHeaderState } = extrasSlice.actions
   
 export default extrasSlice.reducer
