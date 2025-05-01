7f3745ebc27d8c4933fd96b7e7503e854b289fbf
Mitzi Zitler <mitzitler@gmail.com>
2025-05-01 00:55:47 -0400

    custom hook for calling the get registry items spectaculo api

---
+++ src/components/useRegistryItems.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import { useGetRegistryItemsQuery } from '../services/spectaculo.js';
+
+const useRegistryItems = (loginSuccess, loginHeader) => {
+  const [items, setItems] = useState([]);
+  
+  // Only call the API if loginSuccess is true
+  const { data: registry_data, isSuccess } = useGetRegistryItemsQuery(loginHeader, {
+    skip: !loginSuccess, // <- this prevents the query from running until loginSuccess is true
+  });
+
+  useEffect(() => {
+    if (isSuccess && registry_data?.items) {
+      setItems(registry_data.items);
+    }
+  }, [isSuccess, registry_data]);
+
+  return items;
+};
+
+export default useRegistryItems;
