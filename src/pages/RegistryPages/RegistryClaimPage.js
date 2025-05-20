4b2935bc707a611fb4299b62f3447fc6a64a903d
Mitzi Zitler <mitzitler@gmail.com>
2025-05-20 01:00:02 -0400

    Changes to the registry page so that there is a claim section below, with a list of all the users claims. this is the only place a user can unclaim an item

--- src/pages/RegistryPages/RegistryClaimPage.js
+++ src/pages/RegistryPages/RegistryClaimPage.js
@@ -3,17 +3,39 @@ import '../../App.css';
 
 export function RegistryClaimPage({ claimedItems, registryItems }) {
     
-    // claim page is v3
+    function leftJoin(leftArray, rightArray, key) {
+        const result = [];
+      
+        leftArray.forEach(leftItem => {
+          const rightItem = rightArray.find(right => right[key] === leftItem[key]);
+          result.push({ ...leftItem, ...(rightItem || {}) });
+        });
+      
+        return result;
+    }
+
+    let claimedItemsLong = leftJoin(claimedItems, registryItems, 'item_id')
+
+    // make an 'unclaim' button component, which goes on these cards
 
     return (
         <>
-            <h1 className="gift title">Your Claims</h1>
-
+            {claimedItemsLong.length == 1 
+                ? <h1 className="gift title">Your Claim</h1>
+                : <h1 className="gift title">Your Claims</h1>
+            }
             <div className="p-4">
                 <div className="claim-section"> 
-                    {claimedItems.map((claim) => (
-                        <RegistryClaimCard key={claim.id} claim_id={claim.id} 
-                        // registryItems={registryItems} 
+                    {claimedItemsLong.map((claim) => (
+                        <RegistryClaimCard 
+                            key={claim.item_id} 
+                            name={claim.name}
+                            brand={claim.brand}
+                            descr={claim.descr}
+                            img_url={claim.img_url}
+                            link={claim.link}
+                            updated_at={claim.updated_at}
+                            claim_state={claim.claim_state}
                         />
                     ))}
                 </div>
